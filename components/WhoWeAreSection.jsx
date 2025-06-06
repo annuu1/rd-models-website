85858fd715d2968d425d4ffb54adfae6a6ade611
claretax <avninaina223@gmail.com>
2025-06-06 16:25:24 +0530

    ad the lines on image border

--- components/WhoWeAreSection.jsx
+++ components/WhoWeAreSection.jsx
@@ -5,6 +5,7 @@ import Link from "next/link"
 import { SectionHeading } from "./section-heading"
 import { StatCard } from "./stat-card"
 import { AnimatedButton } from "./animated-button"
+import styles from "./WhoWeAreSection.module.css";
 
 export function WhoWeAreSection() {
   return (
@@ -17,6 +18,11 @@ export function WhoWeAreSection() {
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-stretch">
           {/* Left Side: Image */}
           <div className="relative h-full w-full min-h-[350px]">
+            {/* Animated Borders */}
+            <div className={styles["image-border-animated"]}>
+              <div className={styles["border-line"] + " " + styles["left"]}></div>
+              <div className={styles["border-line"] + " " + styles["right"]}></div>
+            </div>
             <Image
               src="/images/rd-brand-img.png"
               alt="Ar. Rohitash Daiya - Founder RD Models"
