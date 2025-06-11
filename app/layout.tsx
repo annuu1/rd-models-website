7bc82c66b41ab9dea1857f901621d1a841b5cc31
claretax <avninaina223@gmail.com>
2025-06-11 16:07:31 +0530

    added the favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -121,7 +121,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
